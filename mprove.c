d4c31066ac700cc23e4d9bb8792758b017865c0a
Edoardo Milotti <milotti@units.it>
2020-05-19 11:18:56 +0200

    initial commit

---
+++ mprove.c
@@ -0,0 +1,1 @@
+#include "nrutil.h"void mprove(a,alud,n,indx,b,x)float **a,**alud,b[],x[];int indx[],n;{	void lubksb();	int j,i;	double sdp;	float *r;	r=vector(0,n);	for (i=0;i<=n;i++) {		sdp = -b[i];		for (j=0;j<=n;j++) sdp += a[i][j]*x[j];		r[i]=sdp;	}	lubksb(alud,n,indx,r);	for (i=0;i<=n;i++) x[i] -= r[i];	free_vector(r,0,n);}
